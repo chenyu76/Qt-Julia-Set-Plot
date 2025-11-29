ae37581a6b0ec4f16a1752caefc3e3a4ce9e78a7
chenyu76 <51879608+chenyu76@users.noreply.github.com>
2025-11-29 11:41:53 +0800

    add shortcut key wasd and arrow keys

--- main.cpp
+++ main.cpp
@@ -11,24 +11,28 @@ int main(int argc, char* argv[]) {
         ColorMap::generateColorMapImage();
     JuliaWidget widget;
 
-    // 创建移动快捷键, 绑定快捷键到一个 lambda 函数
-    QShortcut *shortcutH = new QShortcut(QKeySequence("H"), &widget);
-    QObject::connect(shortcutH, &QShortcut::activated, &widget, &JuliaWidget::moveLeft);
-    QShortcut *shortcutJ = new QShortcut(QKeySequence("J"), &widget);
-    QObject::connect(shortcutJ, &QShortcut::activated, &widget, &JuliaWidget::moveDown);
-    QShortcut *shortcutK = new QShortcut(QKeySequence("K"), &widget);
-    QObject::connect(shortcutK, &QShortcut::activated, &widget, &JuliaWidget::moveUp);
-    QShortcut *shortcutL = new QShortcut(QKeySequence("L"), &widget);
-    QObject::connect(shortcutL, &QShortcut::activated, &widget, &JuliaWidget::moveRight);
-    QShortcut *shortcutU = new QShortcut(QKeySequence("="), &widget);
-    QObject::connect(shortcutU, &QShortcut::activated, &widget, &JuliaWidget::scaleUp);
-    QShortcut *shortcutD = new QShortcut(QKeySequence("-"), &widget);
-    QObject::connect(shortcutD, &QShortcut::activated, &widget, &JuliaWidget::scaleDown);
-    QShortcut *shortcutSave = new QShortcut(QKeySequence("Ctrl+S"), &widget);
-    QObject::connect(shortcutSave, &QShortcut::activated, &widget, [&](){widget.onGenerateButtonClicked(true);});
-    QShortcut *shortcutGen = new QShortcut(QKeySequence("Ctrl+D"), &widget);
-    QObject::connect(shortcutGen, &QShortcut::activated, &widget, [&](){widget.onGenerateButtonClicked(false);});
+    auto bindKeys = [&](auto func, auto... keys) {
+        // C++17 折叠表达式 (Fold Expression)
+        (..., [&](const auto& key) {
+            // 这里显式调用 QKeySequence(key)，此时 key 还是 char*
+            // 在函数体内进行 char* -> QString -> QKeySequence 是合法的
+            auto *shortcut = new QShortcut(QKeySequence(key), &widget);
+            shortcut->setAutoRepeat(true);
+            QObject::connect(shortcut, &QShortcut::activated, &widget, func);
+        }(keys));
+    };
 
+    // 调用时更爽快（注意 func 在前）：
+    bindKeys(&JuliaWidget::moveLeft,  "H", "A", Qt::Key_Left);
+    bindKeys(&JuliaWidget::moveDown,  "J", "S", Qt::Key_Down);
+    bindKeys(&JuliaWidget::moveUp,    "K", "W", Qt::Key_Up);
+    bindKeys(&JuliaWidget::moveRight, "L", "D", Qt::Key_Right);
+
+    bindKeys(&JuliaWidget::scaleUp,   "=", Qt::Key_Plus);
+    bindKeys(&JuliaWidget::scaleDown, "-");
+
+    bindKeys([&](){ widget.onGenerateButtonClicked(true); },  "Ctrl+S");
+    bindKeys([&](){ widget.onGenerateButtonClicked(false); }, "Ctrl+D");
     widget.show();
     return app.exec();
 }
